3d65fe2542504e2a46e34e77ad5a871eb66e9db7
Br1 <brunogiovagnoli@gmail.com>
2018-05-21 01:15:51 -0300

    remove some useless code

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Todos = require('./models/todoModel');
 
 const app = express();
 
@@ -9,7 +8,6 @@ const port = process.env.PORT || 3000;
 const todosRoutes = require('./routes/todos');
 
 app.use('/assets', express.static(__dirname + '/public'));
-app.set('view engine', 'ejs');
 
 mongoose.connect(process.env.DB_PATH || 'mongodb://localhost/node_todo');
 
